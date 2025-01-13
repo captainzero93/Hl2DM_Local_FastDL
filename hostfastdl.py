5725892e0291822a6c6b3a5a23ca816c95897685
redacted <125401979+captainzero93@users.noreply.github.com>
2025-01-13 20:45:23 +0000

    Update hostfastdl.py
    now works correctly

--- hostfastdl.py
+++ hostfastdl.py
@@ -1,18 +1,16 @@
 from flask import Flask, send_from_directory, render_template_string
-from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
-import multiprocessing
 import socket
 import os
 
 app = Flask(__name__)
 
+# Set the fastdl directory path
+FASTDL_DIR = os.path.join(os.getcwd(), 'fastdl')
+
 # Get local IP more reliably
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # This doesn't actually create a connection
         s.connect(('10.255.255.255', 1))
         local_ip = s.getsockname()[0]
     except Exception:
@@ -22,10 +20,6 @@ def get_local_ip():
     return local_ip
 
 local_ip = get_local_ip()
-
-# Create a secure authorizer that only allows downloads
-authorizer = DummyAuthorizer()
-authorizer.add_anonymous(os.getcwd(), perm='elr')  # Only allow download permissions
 
 # HTML template for directory listing
 DIRECTORY_TEMPLATE = '''
@@ -45,7 +39,7 @@ DIRECTORY_TEMPLATE = '''
     <h1>FastDL Directory: {{path}}</h1>
     <ul class="file-list">
     {% for file in files %}
-        <li><a href="{{url_for('serve_files', path=path + '/' + file)}}">{{file}}</a></li>
+        <li><a href="/{{path}}/{{file}}">{{file}}</a></li>
     {% endfor %}
     </ul>
 </body>
@@ -70,40 +64,39 @@ def serve_files(path):
     try:
         # Sanitize path to prevent directory traversal
         requested_path = os.path.normpath(path).lstrip('/')
+        full_path = os.path.join(FASTDL_DIR, requested_path)
         
-        if os.path.isdir(requested_path):
-            files = sorted(os.listdir(requested_path))
+        print(f"Requested path: {requested_path}")
+        print(f"Full path: {full_path}")
+        print(f"File exists: {os.path.exists(full_path)}")
+        
+        if os.path.isdir(full_path):
+            files = sorted(os.listdir(full_path))
             return render_template_string(
                 DIRECTORY_TEMPLATE,
                 path=requested_path,
                 files=files
             )
         else:
-            # Set correct MIME type for bzip2 files (common in Source downloads)
+            directory = os.path.dirname(full_path)
+            filename = os.path.basename(full_path)
+            print(f"Serving from directory: {directory}")
+            print(f"Filename: {filename}")
+            
+            # Set correct MIME type for bzip2 files
             mimetype = None
             if path.endswith('.bz2'):
                 mimetype = 'application/x-bzip2'
-                
+            
             return send_from_directory(
-                '.',
-                requested_path,
+                directory,
+                filename,
                 mimetype=mimetype
             )
     except Exception as e:
+        print(f"Error serving file: {str(e)}")
         return f"Error: {str(e)}", 404
 
-def start_ftp_server():
-    handler = FTPHandler
-    handler.authorizer = authorizer
-    handler.banner = "Source Engine FastDL FTP Server"
-    
-    server = FTPServer(('0.0.0.0', 21), handler)
-    server.serve_forever()
-
 if __name__ == '__main__':
-    # Start FTP server in separate process
-    ftp_server_process = multiprocessing.Process(target=start_ftp_server)
-    ftp_server_process.start()
-    
-    # Start HTTP server
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    print(f"Serving files from: {FASTDL_DIR}")
+    app.run(host='0.0.0.0', port=5000, debug=True)
